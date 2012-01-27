4c90cb98337df7820868d17b1f260e93ad352734
Devil boy <kws512@hotmail.com>
2012-01-26 23:54:33 -0800

    Version 1.4 Restructured code to improve efficiency Fixed the 'onMove only on block change' code Updated to the new Bukkit event system

--- src/me/FieldZ/MushroomJump/MushroomJumpPlayerListener.java
+++ src/me/FieldZ/MushroomJump/MushroomJumpPlayerListener.java
@@ -1,17 +1,14 @@
 package me.FieldZ.MushroomJump;
 
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.Dispenser;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.*;
+import org.bukkit.event.player.*;
 import org.bukkit.util.Vector;
 
-public class MushroomJumpPlayerListener extends PlayerListener{
+public class MushroomJumpPlayerListener implements Listener{
 	public MushroomJump plugin;
 	/**
 	 * Constructor for PlayerListener
@@ -26,26 +23,23 @@ public class MushroomJumpPlayerListener extends PlayerListener{
 	 * If a player moves on to a mushroom block he is catapulted up in the air! 
 	 * @param ev A PlayerMoveEvent object
 	 */
-	@Override
+	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent ev){
-		Player player = ev.getPlayer();
-		if (!plugin.hasPermissions(player, "MushroomJump.affected")) return;
-		
-		if(ev.isCancelled()||ev.getFrom().getBlock().getLocation()==ev.getTo().getBlock().getLocation()) return;
-		
-		Block block = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
-		
-		if(block.getType() == Material.HUGE_MUSHROOM_1){
-			Vector dir = player.getLocation().getDirection().multiply(1.75);
-			Vector vec = new Vector(dir.getX(), 1.5D, dir.getZ());
-			player.setVelocity(vec);
+		if (!ev.getFrom().getBlock().getLocation().equals(ev.getTo().getBlock().getLocation())) {
+			Player player = ev.getPlayer();
+			if (plugin.hasPermissions(player, "MushroomJump.affected")) {
+				Block block = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
+				if (block.getType() == Material.HUGE_MUSHROOM_1) {
+					Vector dir = player.getLocation().getDirection().multiply(1.75);
+					Vector vec = new Vector(dir.getX(), 1.5D, dir.getZ());
+					player.setVelocity(vec);
+				} else if(block.getType() == Material.HUGE_MUSHROOM_2) {
+					Vector dir = player.getLocation().getDirection().multiply(1.75);
+					Vector vec = new Vector(dir.getX(), 2.0D, dir.getZ());
+					player.setVelocity(vec);
+				}
+			}
 		}
-		if(block.getType() == Material.HUGE_MUSHROOM_2){
-			Vector dir = player.getLocation().getDirection().multiply(1.75);
-			Vector vec = new Vector(dir.getX(), 2.0D, dir.getZ());
-			player.setVelocity(vec);
-		}
-		
 	}
 
 }
